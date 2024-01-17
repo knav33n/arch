618f9317ec38150ba73969d98d685b66f32bef3d
knav33n <13643224+knav33n@users.noreply.github.com>
2024-01-17 20:46:10 +0530

    feat: add apps used

---
+++ misc-apps.sh
@@ -0,0 +1,20 @@
+yay -Syu
+
+pacman -S chromium \
+    torbrowser-launcher \
+    gimp \
+    obs-studio \
+    kubectx \
+    vlc \
+    discord \
+    telegram-desktop
+
+yay -S librewolf \
+    brave-bin \
+    google-chrome \
+    betterbird-bin \
+    nvm \
+    slack-desktop \
+
+# aws cli
+# azure cli
